87436efdee2bf88e098ae0f264b3ac859ed722b7
devsyedhaider <devalihaider86@gmail.com>
2026-01-07 13:02:05 +0500

    new files

---
+++ dom.js
@@ -0,0 +1,35 @@
+/*
+
+🌐 What is DOM?
+
+DOM (Document Object Model) is a tree structure of your HTML page that JavaScript can read, change, add, or delete.
+
+👉 JavaScript uses DOM to:
+
+Change text
+
+Change styles
+
+Handle clicks/events
+
+Create/remove elements
+
+Example:
+
+<h1 id="title">Hello</h1>
+
+
+JS can change it using DOM.
+
+🧠 DOM TREE (Basic Idea)
+document
+ └── html
+     ├── head
+     └── body
+         └── h1
+
+
+document is the root object.
+
+
+*/
